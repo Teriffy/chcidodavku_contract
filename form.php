54eb7fc01ad5ee34582572cd0796891d634f1459
teriffy <evzen@leonenko.cz>
2019-04-01 00:10:30 +0200

    Add Form with variables, Add Van Image, Update Index

---
+++ form.php
@@ -0,0 +1,72 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> 
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+              <link rel="stylesheet" href="css/bootstrap-datepicker.min.css">
+
+              <div class="col-10 offset-md-1" style="margin-top: 50px;">
+<h1><strong>SMLOUVA O N&Aacute;JMU DOPRAVN&Iacute;HO PROSTŘEDKU</strong></h1>
+<p>uzavřen&aacute; dle &sect; 630 a n&aacute;sl. Obchodn&iacute;ho z&aacute;kon&iacute;ku</p>
+<p>mezi stranami:<br />YAKUZA s.r.o<br />N&aacute;měst&iacute; 14. Ř&iacute;jna 1307/2<br />150 00 Praha<br />IČ: 24745146<br /> DIČ: CZ24745146<br />jako <strong>pronaj&iacute;matelem</strong> na straně jedn&eacute;</p>
+<p>a</p>
+<p>Jm&eacute;no a př&iacute;jmen&iacute;: <?php echo($_GET["jmeno"]); ?><br />Trval&yacute; pobyt: <?php echo($_GET["pobyt"])?> <br />Telefonn&iacute; č&iacute;slo: <?php echo($_GET["telefon"]);?> <br />RČ/IČ: <?php echo($_GET["rc"]);?><br />Č&iacute;slo OP: <?php echo($_GET["cislo_op"]);?><br />Č&iacute;slo ŘP: <?php echo($_GET["rp"]);?><br />jako <strong>n&aacute;jemcem</strong> na straně druh&eacute;</p>
+<h2><strong>I.&nbsp;</strong><strong>Předmět n&aacute;jmu<br /></strong></h2>
+<p>Pronaj&iacute;matel prohla&scaron;uje, že m&aacute; souhlas vlastn&iacute;ka k pron&aacute;jmu automobilu <?php echo($_GET["vozidlo"]);?></p> 
+<h2><strong>II.<br /></strong></h2>
+<p>Pronaj&iacute;matel pronaj&iacute;m&aacute; touto smlouvou n&aacute;jemci předmět n&aacute;jmu shora označen&yacute; a n&aacute;jemce se zavazuje zaplatit za tento pron&aacute;jem pronaj&iacute;mateli n&aacute;jemn&eacute; dle t&eacute;to smlouvy.</p>
+<h2><strong>III.&nbsp;</strong><strong>Trv&aacute;n&iacute; smlouvy</strong></h2>
+<p>N&aacute;jemn&iacute; smlouva se sjedn&aacute;v&aacute; na dobu určitou, a to ode dne před&aacute;n&iacute; a převzet&iacute; předmětu n&aacute;jmu.<br /><strong>Datum a čas před&aacute;n&iacute;:</strong> <?php echo($_GET["pujceni_od"]." ".$_GET["pujceni_od_time"]);?> hodin&nbsp; <strong>Vr&aacute;cen&iacute; nejpozději do:</strong> <?php echo($_GET["pujceni_do"]." ".$_GET["pujceni_do_time"]);?> hodin</p>
+<h2><strong>IV.&nbsp;</strong><strong>N&aacute;jemn&eacute;</strong></h2>
+<ol style="list-style-type: upper-roman;">
+<li>N&aacute;jemn&eacute; dle t&eacute;to smlouvy čin&iacute; <?php echo($_GET["castka"]);?> Kč s&nbsp;DPH za <?php echo($_GET["pocet_dnu"]);?> ,poč&iacute;naje časem před&aacute;n&iacute; a převzet&iacute; předmětu n&aacute;jmu dle t&eacute;to smlouvy n&aacute;jemcem + 2,- Kč s&nbsp;DPH za každ&yacute; ujet&yacute; kilometr nad limit <strong><?php echo($_GET["max_km"]);?>km</strong>. V&nbsp;př&iacute;padě překročen&iacute; doby n&aacute;jmu vozidla o v&iacute;ce než 1h je &uacute;čtov&aacute;na sazba za dal&scaron;&iacute; započat&yacute; den dle předchoz&iacute; sjednan&eacute; ceny v&nbsp;t&eacute;to smlouvě.<br /><strong>Poč&aacute;tečn&iacute; stav tachometru: &hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;.. km</strong><br /><strong>Konečn&yacute; stav tachometru: &hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip; km</strong></li>
+<li>N&aacute;jemce je povinen uhradit jistinu za pron&aacute;jem vozidla ve v&yacute;&scaron;i <strong>5000</strong>,-<strong>Kč</strong>, kter&aacute; bude vr&aacute;cena při před&aacute;n&iacute; vozidla zpět n&aacute;jemci za předpokladu, že bude vozidlo vr&aacute;ceno v&nbsp;původn&iacute;m stavu, ve kter&eacute;m vozidlo n&aacute;jemce převzal od pronaj&iacute;matele. Stav cel&eacute;ho vozidla (exteri&eacute;r i interi&eacute;r) bude vždy zaznamen&aacute;n před jeho zapůjčen&iacute;m na videoz&aacute;znam. Ve&scaron;ker&eacute; po&scaron;kozen&eacute; č&aacute;sti budou tak&eacute; pops&aacute;ny v&nbsp;dodatku t&eacute;to smlouvy. Prohl&iacute;dka vozu a video z&aacute;znam budou vždy provedeny za př&iacute;tomnosti obou stran (n&aacute;jemce i pronaj&iacute;matele) a stav vozidla bude oběmi stranami pops&aacute;n a potvrzen v&nbsp;t&eacute;to smlouvě.</li>
+<li>N&aacute;jemn&eacute; je splatn&eacute; vždy před pron&aacute;jmem vozidla a to v&nbsp;hotovosti k&nbsp;ruk&aacute;m pronaj&iacute;matele.</li>
+</ol>
+<h2><strong>V.&nbsp;</strong><strong>Povinnosti pronaj&iacute;matele</strong></h2>
+<ol style="list-style-type: upper-roman;">
+<li>Pronaj&iacute;matel se zavazuje předat předmět n&aacute;jmu n&aacute;jemci při podpisu t&eacute;to smlouvy v&nbsp;ulici Partyz&aacute;nsk&aacute; 1032, Česk&aacute; L&iacute;pa 47001, popř&iacute;padě dle dohody.</li>
+<li>Předmět n&aacute;jmu mus&iacute; b&yacute;t ke dni jeho před&aacute;n&iacute; n&aacute;jemci způsobil&yacute; k&nbsp;obvykl&eacute;mu provozu.</li>
+<li>Pronaj&iacute;matel se zavazuje předat n&aacute;jemci spolu s&nbsp;předmětem n&aacute;jmu i ve&scaron;ker&eacute; potřebn&eacute; doklady, zejm&eacute;na osvědčen&iacute; o technick&eacute;m průkazu a doklad o z&aacute;konn&eacute;m poji&scaron;těn&iacute; vozidla.</li>
+</ol>
+<h2><strong>VI.&nbsp;</strong><strong>Povinnosti n&aacute;jemce</strong></h2>
+<ol style="list-style-type: upper-roman;">
+<li>N&aacute;jemce nen&iacute; opr&aacute;vněn poskytnout předmět n&aacute;jmu k&nbsp;už&iacute;v&aacute;n&iacute; třet&iacute; osobě ani jej d&aacute;le pronajmout.</li>
+<li>Dotankov&aacute;n&iacute; vozidla do pln&eacute; n&aacute;drže (vozidlo před&aacute;no s&nbsp;plnou n&aacute;drž&iacute;). V&nbsp;př&iacute;padě nedotankov&aacute;n&iacute; vozidla je &uacute;čtov&aacute;n poplatek. Za 1l paliva 40 Kč.</li>
+<li>N&aacute;jemce se zavazuje použ&iacute;vat předmět n&aacute;jmu k&nbsp;&uacute;čelům, k&nbsp;nimž obvykle slouž&iacute;. Předmět n&aacute;jmu nelze použ&iacute;t jako z&aacute;stavu.</li>
+<li>N&aacute;jemce je opr&aacute;vněn použ&iacute;vat předmět n&aacute;jmu mimo &uacute;zem&iacute; ČR pouze s&nbsp;předchoz&iacute;m p&iacute;semn&yacute;m souhlasem pronaj&iacute;matele.</li>
+<li>N&aacute;jemce je povinen nahl&aacute;sit stav tachometru vždy k&nbsp;16. dni každ&eacute;ho měs&iacute;ce pron&aacute;jmu pro potřeby &uacute;čtov&aacute;n&iacute; n&aacute;jmu na tel. 776/776 366.</li>
+<li>N&aacute;jemce je povinen dostavit se s&nbsp;vozidlem, kter&eacute; je předmětem n&aacute;jmu k&nbsp;pravideln&eacute; servisn&iacute; prohl&iacute;dce po předchoz&iacute; domluvě s&nbsp;pronaj&iacute;matelem. V&nbsp;př&iacute;padě, že tak neučin&iacute;, je n&aacute;jemce odpovědn&yacute; za &scaron;kody vznikl&eacute; v&nbsp;souvislosti s&nbsp;nedostaven&iacute;m se k&nbsp;pravideln&eacute; prohl&iacute;dce.</li>
+<li>V&nbsp;př&iacute;padě &scaron;kodn&eacute; ud&aacute;losti je n&aacute;jemce povinen zavolat Policii ČR a sepsat protokol o způsoben&eacute; &scaron;kodě a informovat pronaj&iacute;matele na tel. 776 776&nbsp;366.</li>
+<li>V&nbsp;př&iacute;padě pokut a &uacute;hrady jin&yacute;ch sankčn&iacute;ch poplatků třet&iacute;m stran&aacute;m, způsoben&yacute;ch n&aacute;jemcem vozidla je povinen n&aacute;jemce tyto poplatky uhradit bez zbytečn&eacute;ho prodlen&iacute; pronaj&iacute;mateli vozidla, kter&yacute; n&aacute;sledně zajist&iacute; jejich &uacute;hradu tomu, kdo tyto poplatky předepsal.</li>
+<li>Po skončen&iacute; n&aacute;jmu se n&aacute;jemce zavazuje předat předmět n&aacute;jmu pronaj&iacute;mateli na provozovně v&nbsp;Partyz&aacute;nsk&eacute; ulici 1032 v&nbsp;Česk&eacute; L&iacute;pě do 24h od zapůjčen&iacute;, popř. dle dohody. Nejpozději do 17.00 h. A to ve stavu, v&nbsp;jak&eacute;m jej převzal s&nbsp;přihl&eacute;dnut&iacute;m k&nbsp;obvykl&eacute;mu opotřeben&iacute;.</li>
+<li>V&nbsp;př&iacute;padě prodlen&iacute; n&aacute;jemce s&nbsp;vr&aacute;cen&iacute;m předmětu n&aacute;jmu je n&aacute;jemce d&aacute;le povinen hradit sjednan&eacute; denn&iacute; n&aacute;jemn&eacute;. A d&aacute;le je povinen pronaj&iacute;mateli uhradit smluvn&iacute; pokutu ve v&yacute;&scaron;i 500,- Kč za každ&yacute; den prodlen&iacute;.</li>
+<li>N&aacute;jemce je povinen při provozu pronajat&eacute;ho vozidla dodržovat v&scaron;echny platn&eacute; předpisy, zejm&eacute;na pravidla silničn&iacute;ho provozu, a poč&iacute;nat si tak, aby nedo&scaron;lo ke&nbsp;&scaron;kodě na majetku a zdrav&iacute;.</li>
+<li>N&aacute;jemce se zavazuje při ztr&aacute;tě kl&iacute;če od vozidla pronaj&iacute;mateli uhradit ve&scaron;ker&eacute; n&aacute;klady spojen&eacute; s&nbsp;n&aacute;pravou tohoto stavu.</li>
+<li>N&aacute;jemce je povinen uhradit ve&scaron;ker&eacute; n&aacute;klady spojen&eacute; s&nbsp;likvidac&iacute; &scaron;kody, kter&aacute; vznikla na vozidle nebo v&nbsp;souvislosti s&nbsp;jeho provozem v&nbsp;obdob&iacute; pron&aacute;jmu, pokud nedodrž&iacute; sjednan&eacute; podm&iacute;nky.</li>
+<li>N&aacute;jemce je povinen při dopravn&iacute; nehodě způsoben&eacute; jeho vinou, při nezji&scaron;těn&iacute; pachatele nehody nebo při jak&eacute;mkoliv po&scaron;kozen&iacute; vozidla a jeho př&iacute;slu&scaron;enstv&iacute; či v&nbsp;př&iacute;padech, kdy nelze uplatnit n&aacute;hradu &scaron;kody z&nbsp;jin&eacute;ho poji&scaron;těn&iacute; zaplatit spolu&uacute;čast ve v&yacute;&scaron;i 5%, minim&aacute;lně v&scaron;ak 5000 Kč (nedohodnou-li se obě strany jinak) a zaplatit odtah do pronaj&iacute;matelem určen&eacute; opravny. Popř&iacute;padě po dohodě obou smluvn&iacute;ch stran do s&iacute;dla pronaj&iacute;matele.</li>
+<li>Při kr&aacute;deži pronajat&eacute;ho vozidla je n&aacute;jemce povinen zaplatit pronaj&iacute;mateli spolu&uacute;čast ve v&yacute;&scaron;i 5% z&nbsp;pořizovac&iacute; ceny vozidla, minim&aacute;lně v&scaron;ak 5000 Kč.</li>
+<li>V&nbsp;př&iacute;padě zneči&scaron;těn&iacute; vozidla je n&aacute;jemce povinen uhradit n&aacute;klady na jeho či&scaron;těn&iacute;.</li>
+<li>N&aacute;jemce je povinen zdržet se kouřen&iacute; ve vozidle. V&nbsp;př&iacute;padě, že ve vozidle bude n&aacute;jemce či třet&iacute; osoba kouřit, je n&aacute;jemce povinen uhradit smluvn&iacute; pokutu ve v&yacute;&scaron;i 500,- Kč.</li>
+</ol>
+<h2><strong>VII.&nbsp;</strong><strong>Poji&scaron;těn&iacute;</strong></h2>
+<p>Vozidlo je havarijně poji&scaron;těno s&nbsp;5% spolu&uacute;čast&iacute; n&aacute;jemce, minim&aacute;lně v&scaron;ak 5000 Kč (plat&iacute; na ve&scaron;ker&eacute; po&scaron;kozen&iacute; vozidla vč. po&scaron;kozen&iacute; čeln&iacute;ho skla). Ve&scaron;ker&aacute; poji&scaron;těn&iacute; stran předmětu n&aacute;jmu zaji&scaron;ťuje pronaj&iacute;matel a v&nbsp;př&iacute;padě pojistn&eacute; ud&aacute;losti mu tak&eacute; n&aacute;lež&iacute; v&scaron;echna pojistn&aacute; plněn&iacute;.</p>
+<h2><strong>VIII.&nbsp;</strong><strong>Skončen&iacute; n&aacute;jmu</strong></h2>
+<p>N&aacute;jem dle t&eacute;to smlouvy skonč&iacute; v&nbsp;souladu s&nbsp;ustanoven&iacute;m čl. III. t&eacute;to smlouvy, d&aacute;le zničen&iacute;m předmětu n&aacute;jmu, př&iacute;padně p&iacute;semnou v&yacute;pověd&iacute; jedn&eacute; ze smluvn&iacute;ch stran, když v&yacute;povědn&iacute; doba čin&iacute; 30 dnů a poč&iacute;n&aacute; běžet ode dne n&aacute;sleduj&iacute;c&iacute;ho po jej&iacute;m doručen&iacute; druh&eacute; smluvn&iacute; straně. P&iacute;semnou v&yacute;pověd&iacute; lze ukončit pouze v&nbsp;př&iacute;padě hrub&eacute;ho poru&scaron;en&iacute; někter&eacute;ho z&nbsp;ustanoven&iacute; t&eacute;to smlouvy či ustanoven&iacute; z&aacute;kona jednou ze smluvn&iacute;ch stran, přičemž hrub&yacute;m poru&scaron;en&iacute;m se rozum&iacute; takov&eacute; jedn&aacute;n&iacute; smluvn&iacute; strany, kter&eacute; znemožn&iacute; ř&aacute;dnou realizaci t&eacute;to smlouvy.</p>
+<h2><strong>IX.&nbsp;</strong><strong>Z&aacute;věrečn&aacute; ustanoven&iacute;</strong></h2>
+<ol style="list-style-type: upper-roman;">
+<li>Tato smlouva nab&yacute;v&aacute; platnosti dnem podpisu smluvn&iacute;mi stranami.</li>
+<li>Ve&scaron;ker&eacute; změny t&eacute;to smlouvy je možn&eacute; učinit jen po vz&aacute;jemn&eacute; dohodě smluvn&iacute;ch stran a v&nbsp;p&iacute;semn&eacute; formě.</li>
+<li>V&nbsp;př&iacute;padě, že někter&eacute; ustanoven&iacute; t&eacute;to smlouvy je nebo se stane neplatn&yacute;m, zůst&aacute;vaj&iacute; ostatn&iacute; ustanoven&iacute; t&eacute;to smlouvy v&nbsp;platnosti. Strany se zavazuj&iacute; nahradit neplatn&eacute; ustanoven&iacute; smlouvy ustanoven&iacute;m jin&yacute;m, kter&eacute; nejl&eacute;pe sv&yacute;m obsahem a smyslem odpov&iacute;d&aacute; obsahu a smyslu ustanoven&iacute; původn&iacute;ho.</li>
+<li>Tato smlouva se vyhotovuje ve dvou vyhotoven&iacute;ch, přičemž každ&aacute; ze stran obdrž&iacute; po jednom.</li>
+<li>Dle čl. 6 odst. 1 p&iacute;sm. a) a čl. 7 ​nař&iacute;zen&iacute; Evropsk&eacute;ho parlamentu a Rady (EU) 2016/679 ze dne 27. dubna 2016, o ochraně fyzick&yacute;ch osob v souvislosti se zpracov&aacute;n&iacute;m osobn&iacute;ch &uacute;dajů a o voln&eacute;m pohybu těchto &uacute;dajů a o zru&scaron;en&iacute; směrnice 95/46/EC (&bdquo;GDPR​&ldquo;).</li>
+<li>Podpisem t&eacute;to smlouvy n&aacute;jemce uděluje souhlas se zpracov&aacute;n&iacute;m osobn&iacute;ch &uacute;dajů potřebn&yacute;ch k zapůjčen&iacute; vozu.</li>
+</ol>
+<h2><strong>X.&nbsp;</strong><strong>Stav vozidla</strong></h2>
+<img src="img/dodavka.jpg" alt="dodávka"></img>
+<p>Při předání</p>
+<p>V&nbsp;Česk&eacute; L&iacute;pě dne <?php echo($_GET["pujceni_od"]) ?></p>
+<p>pronaj&iacute;matel:&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;</p>
+<p>n&aacute;jemce:&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;</p>
+
+<p>Při vrácení</p>
+<p>pronaj&iacute;matel:&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;</p>
+<p>n&aacute;jemce:&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;&hellip;</p>
+</div>
